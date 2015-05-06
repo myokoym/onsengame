3c9ac9feef1aba083ba9dee4576af5bafe33dc87
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-05-06 23:56:05 +0900

    Add generate command

--- lib/onsengame/command.rb
+++ lib/onsengame/command.rb
@@ -1,3 +1,4 @@
+require "fileutils"
 require "thor"
 require "onsengame/version"
 require "onsengame/window"
@@ -14,6 +15,27 @@ module Onsengame
     def demo
       @window = Window.new(options.dup)
       @window.show
+    end
+
+    desc "generate NAME", "Generate project"
+    def generate(name)
+      raise "Error: File exists." if File.exist?(name)
+
+      onsengame_dir = File.expand_path("../../..", __FILE__)
+      paths = Dir.glob("#{onsengame_dir}/*")
+      paths << ".gitignore"
+      paths.each do |path_from|
+        FileUtils.mkdir_p(name)
+        path_to = File.join(name, File.basename(path_from))
+        FileUtils.cp_r(path_from, path_to)
+      end
+
+      FileUtils.cd(name) do
+        system("git", "init")
+        system("git", "add", ".")
+        system("git", "commit -m 'Initial commit'")
+        system("rename_project", "onsengame", name)
+      end
     end
   end
 end
--- onsengame.gemspec
+++ onsengame.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency("gosu")
   spec.add_runtime_dependency("thor")
+  spec.add_runtime_dependency("inplace")
 
   spec.add_development_dependency("test-unit")
   spec.add_development_dependency("bundler")
